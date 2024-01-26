0f3a85eddbbb9d238faa4f53f9c9d6b0f15d4707
stan <stitrzv@proton.me>
2024-01-26 13:41:19 +0200

    deleted

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -5,6 +5,9 @@ import { Toaster } from "./ui/toaster";
 import algoliasearch from "algoliasearch/lite";
 import { InstantSearch } from "react-instantsearch";
 
+// console.log("d", import.meta.env.VITE_ALGOLIA_APPLICATION_ID);
+// console.log("e", import.meta.env.VITE_ALGOLIA_SEARCH_ONLY_API_KEY);
+
 const ALGOLIA_APPLICATION_ID = import.meta.env.VITE_ALGOLIA_APPLICATION_ID;
 const ALGOLIA_SEARCH_ONLY_API_KEY = import.meta.env
   .VITE_ALGOLIA_SEARCH_ONLY_API_KEY;
